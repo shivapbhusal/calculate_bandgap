aaa4e98aeb7efd83f44f5aa11becf5223662f80a
shivapbhusal <civabhusal@gmail.com>
2018-05-31 18:58:56 -0400

    modify stringToFloat

--- compute_bandgap.py
+++ compute_bandgap.py
@@ -4,10 +4,20 @@ A python script to compute bandgap in zinc oxide
 
 import sys
 
+def stringToFloat(myStr):
+    print(myStr)
+    if 'E+' in myStr:
+        myStr=myStr.split('E+')
+        return float(myStr[0])*pow(10,float(myStr[1]))
+    else:
+        myStr=myStr.split('E-')
+        return float(myStr[0])* pow(10,float(myStr[1])*-1)
+
 doscarFile=open(sys.argv[1])
 
 for lines in doscarFile:
     lines=lines.strip().split('  ')
     print(lines)
+    print(stringToFloat(lines[1]))
 
 doscarFile.close()
